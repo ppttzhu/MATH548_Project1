56ab7ad7183e4e3e997487beaf336bc80c7d5858
ppttzhu <44450843+ppttzhu@users.noreply.github.com>
2018-10-25 12:01:39 -0500

    Update Project1_toy code.py

--- Project1_toy code.py
+++ Project1_toy code.py
@@ -9,6 +9,21 @@ from scipy.stats import norm
 
 # Binominal tree
 
+def binominal_tree_call_analytic(s: float, u: float, d: float, r: float, step: int) -> float:
+    """
+    triangle !~~
+    private binominal tree support function to calculate hedging ratio on each node
+    :param s: list of spot price of the underlying asset. Length 2, upward and downward side.
+    :param x: list of value of the contingent claim. Length 2, upward and downward side.
+    :param r: risk free rate (annual rate, expressed in terms of compounding)
+    :param t: time interval of binominal tree (expressed in years)
+    :return: list of hedging strategy of the contingent claim. Length 2, (H0, H1)
+    """
+
+    call = 0
+
+    return call
+
 def binominal_tree_hedging(s: list, x: list, r: float, t: float) -> list:
     """
     private binominal tree support function to calculate hedging ratio on each node
@@ -18,10 +33,10 @@ def binominal_tree_hedging(s: list, x: list, r: float, t: float) -> list:
     :param t: time interval of binominal tree (expressed in years)
     :return: list of hedging strategy of the contingent claim. Length 2, (H0, H1)
     """
-    H1 = (x[1] - x[0]) / (s[1] - s[0])
-    H0 = (x[1] - s[1] * H1) / math.pow((1 + r), t)
+    h1 = (x[1] - x[0]) / (s[1] - s[0])
+    h0 = (x[1] - s[1] * h1) / math.pow((1 + r), t)
 
-    hedging = [H0, H1]
+    hedging = [h0, h1]
 
     return hedging
 
@@ -83,6 +98,32 @@ def bs_formula_d2(s: float, k: float, t: float, r: float, sigma: float) -> float
 
     return d2
 
+
+# Volatility calculation
+
+def volatility_historical_price(s: list, multiplied_factor: int):
+    """
+    private support function to calculate volatility for underlying asset
+    :param s: list of historical spot price of the underlying asset.
+    :param multiplied_factor: number of business days for the corresponding frequency of historical data.
+    E.g. for daily, 250 or 260. For weekly, 52.
+    :return: annualized volatility
+    """
+
+    # insufficient length of data
+    if len(s) < 2:
+        return 0
+
+    # logarithm of historical price
+    log_s = []
+    for i in range(1, len(s)):
+        log_s.append(math.log(s[i - 1] / s[i]))
+
+    vol = np.std(log_s, ddof=1) * math.sqrt(multiplied_factor)
+
+    return vol
+
+
 # ------------Data Retrieval & Prep------------
 
 # ------------Inputs & Outputs------------
@@ -90,3 +131,8 @@ def bs_formula_d2(s: float, k: float, t: float, r: float, sigma: float) -> float
 # ------------Print Solution------------
 
 # -------------Graph Solution------------
+
+# -------------Test------------
+s1 = [2, 1, 4, 5, 6]
+hh1 = volatility_historical_price(s1, 250)
+print(hh1)
