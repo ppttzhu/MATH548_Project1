9a7a9019a991f3387c939e2f7e78fd93ea740541
ppttzhu <44450843+ppttzhu@users.noreply.github.com>
2018-10-25 10:19:21 -0500

    Update Project1_toy code.py

--- Project1_toy code.py
+++ Project1_toy code.py
@@ -7,6 +7,27 @@ from scipy.stats import norm
 
 # ________________FUNCTIONS______________________
 
+# Binominal tree
+
+def binominal_tree_hedging(s: list, x: list, r: float, t: float) -> list:
+    """
+    private binominal tree support function to calculate hedging ratio on each node
+    :param s: list of spot price of the underlying asset. Length 2, upward and downward side.
+    :param x: list of value of the contingent claim. Length 2, upward and downward side.
+    :param r: risk free rate (annual rate, expressed in terms of compounding)
+    :param t: time interval of binominal tree (expressed in years)
+    :return: list of hedging strategy of the contingent claim. Length 2, (H0, H1)
+    """
+    H1 = (x[1] - x[0]) / (s[1] - s[0])
+    H0 = (x[1] - s[1] * H1) / math.pow((1 + r), t)
+
+    hedging = [H0, H1]
+
+    return hedging
+
+
+# Black–Scholes formula to test in continuous time
+
 def bs_formula_call(s: float, k: float, t: float, r: float, sigma: float) -> float:
     """
     Black–Scholes formula to price call option
@@ -47,7 +68,7 @@ def bs_formula_put(s: float, k: float, t: float, r: float, sigma: float) -> floa
 
 def bs_formula_d1(s: float, k: float, t: float, r: float, sigma: float) -> float:
     """
-    Black–Scholes formula support function
+    private Black–Scholes formula support function
     """
     d1 = (math.log(s / k) + (r + sigma * sigma / 2) * t) / sigma * math.sqrt(t)
 
@@ -56,7 +77,7 @@ def bs_formula_d1(s: float, k: float, t: float, r: float, sigma: float) -> float
 
 def bs_formula_d2(s: float, k: float, t: float, r: float, sigma: float) -> float:
     """
-    Black–Scholes formula support function
+    private Black–Scholes formula support function
     """
     d2 = (math.log(s / k) + (r - sigma * sigma / 2) * t) / sigma * math.sqrt(t)
 
