39d6bf8d20ba13c01e6f8b3297c3ea83ed1c712b
ppttzhu <44450843+ppttzhu@users.noreply.github.com>
2018-11-05 21:51:55 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -12,7 +12,7 @@
 
 from pandas_datareader.data import Options
 from pricingengine import *
-from pandas_datareader import data, wb
+from pandas_datareader import data
 from pinance import Pinance
 from dateutil.relativedelta import relativedelta
 import csv
@@ -23,21 +23,41 @@ def main():
 
     stock_name = 'TWTR'
     data_source_price = 'yahoo'
-    data_source_dividend= 'yahoo-dividends'
+    data_source_dividend = 'yahoo-dividends'
     maturity_string_test = "2019-03-15"
     maturity_test = datetime.datetime.strptime(maturity_string_test, "%Y-%m-%d")
-    n_test = 10  # step of binimial tree
-    IS_PRINT = 1
+    pricing_date_string_test = "2018-11-05"
+    pricing_date_test = datetime.datetime.strptime(pricing_date_string_test, "%Y-%m-%d")
+    IS_CALCULATOR = 0  # use a calculator or not
 
-    # ----------------user-defined product data---------------------
+    # ----------------product data---------------------
 
-    pricing_date_string_test = "2018-10-31"
-    pricing_date_test = datetime.datetime.strptime(pricing_date_string_test, "%Y-%m-%d")  # "2018-09-30"
+    # Get options for calibration, source: bbg
+    csv_file = open("[input]Options_for_calibrate_" + pricing_date_string_test + ".csv", "r")
+    reader = csv.reader(csv_file)
 
-    cp_test = CallPutType.put
-    exercise_european = ExerciseType.european
-    exercise_american = ExerciseType.american
-    k_test = 20.0
+    options_for_calibrate_list = []
+    options_for_calibrate_price_list = []
+    options_list = []
+    options_price_list = []
+
+    for item in reader:
+        # Drop first line
+        if reader.line_num == 1:
+            continue
+
+        maturity_date = datetime.datetime.strptime(item[2], "%Y-%m-%d")
+        option = Option(item[0], float(item[1]), maturity_date, CallPutType(int(item[3])), ExerciseType(int(item[4])))
+        options_for_calibrate_list.append(option)
+        options_for_calibrate_price_list.append(float(item[5]))
+        if item[2] == maturity_string_test:
+            options_list.append(option)
+            options_price_list.append(float(item[5]))
+
+    csv_file.close()
+
+    if IS_CALCULATOR:
+        print("windows")
 
     # ----------------market data---------------------
 
@@ -46,23 +66,21 @@ def main():
     historical_start_date1 = pricing_date_test - relativedelta(years=1)
     historical_start_date_test = min(historical_start_date1, historical_start_date2)
 
-    stock_price_list = data.DataReader(name=stock_name, data_source=data_source_price, start=historical_start_date_test, end=pricing_date_test)
+    stock_price_list = data.DataReader(name=stock_name, data_source=data_source_price, start=historical_start_date_test,
+                                       end=pricing_date_test)
     s_history_test = stock_price_list['Adj Close']
     s_test = stock_price_list['Adj Close'][pricing_date_test]
 
-    dividend_list = data.DataReader(name=stock_name, data_source=data_source_dividend, start=historical_start_date1, end=pricing_date_test)
+    dividend_list = data.DataReader(name=stock_name, data_source=data_source_dividend, start=historical_start_date1,
+                                    end=pricing_date_test)
     if not dividend_list.empty:
-        b_test = sum(list(dividend_list['value']))/s_test
+        b_test = sum(list(dividend_list['value'])) / s_test
     else:
         b_test = 0
 
-    # Broken Link for Option Quote
-    # getOptions = Options('AAPL')
-    # all_data = getOptions.get_all_data()
-
     # Daily Treasury Yield Curve Rates for Risk Free Rate, Data source:
     # https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yieldYear&year=2018
-    csv_file = open("RiskFreeRate_"+pricing_date_string_test+".csv", "r")
+    csv_file = open("[input]RiskFreeRate_" + pricing_date_string_test + ".csv", "r")
     reader = csv.reader(csv_file)
 
     risk_free_rate_x = []
@@ -77,62 +95,73 @@ def main():
     risk_free_rate = [risk_free_rate_x, risk_free_rate_y]
     csv_file.close()
 
-    stock = Pinance(stock_name)
-    if cp_test == CallPutType.call:
-        stock.get_options(maturity_string_test, 'C', k_test)
-    else:
-        stock.get_options(maturity_string_test, 'P', k_test)
-    market_price_today = stock.options_data['lastPrice']
+    if IS_CALCULATOR:
+        stock = Pinance(stock_name)
+        if options_list[0].call_put_type == CallPutType.call:
+            stock.get_options(maturity_string_test, 'C', options_list[0].strike)
+        else:
+            stock.get_options(maturity_string_test, 'P', options_list[0].strike)
+        market_price_today = stock.options_data['lastPrice']
 
     # ----------------performing pricing---------------------
 
-    option_european = Option(stock_name, k_test, maturity_test, cp_test, exercise_european)
+    pricing_engine = OptionPricingEngine(pricing_date_test, options_list[0])
+    parameters = pricing_engine.calibrate(s_test, risk_free_rate, b_test, s_history_test, options_for_calibrate_list,
+                                          options_for_calibrate_price_list)
 
-    option_european1 = Option(stock_name, 11, maturity_test, cp_test, exercise_european)
-    option_european2 = Option(stock_name, 9, maturity_test, cp_test, exercise_european)
-    option_european3 = Option(stock_name, 10.5, maturity_test, cp_test, exercise_european)
-
-    options_test = [option_european1, option_european2, option_european3]
-    market_price_list_test = [0.5, 2, 1]
-
-    pricing_engine_european_bi = OptionPricingEngine(pricing_date_test, option_european)
+    npv_list = []
+    for option in options_list:
+        pricing_engine = OptionPricingEngine(pricing_date_test, option)
+        npv = pricing_engine.npv(s_test, risk_free_rate, b_test, parameters[0], parameters[1])
+        npv_list.append(npv)
 
     # ----------------printing results---------------------
 
-    npv = pricing_engine_european_bi.npv(s_test, risk_free_rate, b_test, s_history_test, n_test, options_test, market_price_list_test)
+    if not parameters[0] == []:
+        print("sigma = %f" % parameters[0])
+    if parameters[1]:
+        print("up = %f" % parameters[1][0])
+        print("down = %f" % parameters[1][1])
+        print("q_up = %f" % parameters[1][2])
+        print("q_down = %f" % parameters[1][3])
 
-    print("The price of European option by binomial tree is %f" % npv[0])
+    if not IS_CALCULATOR:
+        with open("[output]npv_list.csv", "w", newline='') as csvfile:
+            result_writer = csv.writer(csvfile, delimiter=',', quotechar=',')
+            for i in range(len(options_list)):
+                result_writer.writerow([options_list[i].product_id, npv_list[i][0], options_price_list[i],
+                                        npv_list[i][0] - options_price_list[i]])
 
-    print("options data :  ", stock.options_data)
+    if IS_CALCULATOR:
+        print("price = " + npv[0])
 
-    forward = Forward(stock_name, k_test, maturity_test)
-    pricing_engine_forward = ForwardPricingEngine(pricing_date_test, forward)
-    print("The price of forward is %f" % pricing_engine_forward.npv(s_test, risk_free_rate, b_test))
-
-    # h0_tree, h1_tree, s_tree, bond_tree, option_tree
-
-    # print Hedging strategy in csv files
-    if len(npv) > 1 and IS_PRINT:
-        with open("[ouput]h0_tree.csv", "w", newline='') as csvfile:
+    # export hedging strategy in csv files
+    if len(npv) > 1 and IS_CALCULATOR:
+        with open("[output]h0_tree.csv", "w", newline='') as csvfile:
             result_writer = csv.writer(csvfile, delimiter=',', quotechar=',')
             for line in npv[1]:
                 result_writer.writerow(line)
-        with open("[ouput]h1_tree.csv", "w", newline='') as csvfile:
+        with open("[output]h1_tree.csv", "w", newline='') as csvfile:
             result_writer = csv.writer(csvfile, delimiter=',', quotechar=',')
             for line in npv[2]:
                 result_writer.writerow(line)
-        with open("[ouput]s_tree.csv", "w", newline='') as csvfile:
+        with open("[output]s_tree.csv", "w", newline='') as csvfile:
             result_writer = csv.writer(csvfile, delimiter=',', quotechar=',')
             for line in npv[3]:
                 result_writer.writerow(line)
-        with open("[ouput]bond_tree.csv", "w", newline='') as csvfile:
+        with open("[output]bond_tree.csv", "w", newline='') as csvfile:
             result_writer = csv.writer(csvfile, delimiter=',', quotechar=',')
             for line in npv[4]:
                 result_writer.writerow(line)
-        with open("[ouput]option_tree.csv", "w", newline='') as csvfile:
+        with open("[output]option_tree.csv", "w", newline='') as csvfile:
             result_writer = csv.writer(csvfile, delimiter=',', quotechar=',')
             for line in npv[5]:
                 result_writer.writerow(line)
+
+    # forward = Forward(stock_name, k_test, maturity_test)
+    # pricing_engine_forward = ForwardPricingEngine(pricing_date_test, forward)
+    # print("The price of forward is %f" % pricing_engine_forward.npv(s_test, risk_free_rate, b_test))
+
 
 if __name__ == "__main__":
     main()
